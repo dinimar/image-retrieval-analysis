1979cfada5ea8c3af75210956c2d42bb8f446370
Dinir Imameev <dinir.imameev@gmail.com>
2020-09-27 18:53:32 +0000

    feat(): add feature extraction

--- digix_train.py
+++ digix_train.py
@@ -11,13 +11,22 @@ from PIL import Image
 
 TRAIN = 'cars_train/cars_train'
 
-def image_loader(loader, image_name):
-    """load image, returns cuda tensor"""
-    image = Image.open(image_name)
-    image = loader(image).float()
-    #image = Variable(image, requires_grad=True)
-    image = image.unsqueeze(0)  #this is for VGG, may not be needed for ResNet
-    return image  #assumes that you're using GPU
+def image_loader(filename):
+    input_image = Image.open(filename)
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    input_tensor = preprocess(input_image)
+    input_batch = input_tensor.unsqueeze(0) # create a mini-batch as expected by the model
+
+    if torch.cuda.is_available():
+        input_batch = input_batch.to('cuda')
+
+    return input_batch
+
 
 model=resnext101_32x48d_wsl(progress=True) # example with the ResNeXt-101 32x48d 
 
@@ -28,14 +37,21 @@ for k in model_dict.keys():
     if(('module.'+k) in pretrained_dict.keys()):
         model_dict[k]=pretrained_dict.get(('module.'+k))
 model.load_state_dict(model_dict)
-
-feat_ext_model = nn.Sequential(*list(model.modules())[:-1])
+model.eval()
 
 # extract feauture vectors from images
-loader = transforms.Compose([ transforms.ToTensor()])
-# train_set =datasets.ImageFolder('train')
+images = [join(TRAIN, f) for f in listdir(TRAIN) if isfile(join(TRAIN, f))]
 
+if torch.cuda.is_available():
+    model.to('cuda')
 
-onlyfiles = [f for f in listdir(TRAIN) if isfile(join(TRAIN, f))]
-print(onlyfiles)
+for img in images:
+    input_batch = image_loader(img)
 
+    with torch.no_grad():
+        output = model(input_batch)
+
+    print(output[0])
+    print(output[0].size())
+
+    break
