d3e536f2e7426fdae24a7be376c9f4eec62c974a
Dinir Imameev <dinir.imameev@gmail.com>
2020-10-02 20:14:37 +0300

    feat(digix_data): add path

--- digix_data.py
+++ digix_data.py
@@ -1,8 +1,9 @@
 import yaml
 import wget
+import os
 
-def download_datasets():
-    with open('digix.yaml') as f:
+def download_datasets(yaml_path):
+    with open(os.path.join(yaml_path, 'digix.yaml')) as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
         
         wget.download(data['train_data'])
