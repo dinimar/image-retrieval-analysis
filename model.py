d21dca230125e9a5467ddea78dae9f5c5194962d
Dinir Imameev <dinir.imameev@gmail.com>
2020-10-03 20:29:58 +0300

    refactor(): add train script

--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-import argparse
 import torchvision
 from torch import nn
 from torch.optim.lr_scheduler import ReduceLROnPlateau
@@ -6,8 +5,6 @@ from sklearn.metrics import average_precision_score, accuracy_score
 import torch
 from PIL import Image
 import numpy as np
-
-from dataset import create_dataloader
 
 class FineTuneModel(nn.Module):
     def __init__(self, original_model, num_classes):
@@ -106,30 +103,3 @@ class ModelInterface:
         return pred_labels, pred[0][pred_labels]
         
 
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='train loop for classification net.')
-    parser.add_argument('csv_file', type=str, help='path to csv file that contains dataset info in format:\n1.jpg,0\n2.jpg,1')
-    parser.add_argument('root_dir', type=str, help='path to root dir with dataset')
-    parser.add_argument('path_to_model', type=str, default='', help='path to pretraned model')
-    parser.add_argument('num_classes', type=int, help='num classes in dataset')
-
-    args = parser.parse_args()
-    
-    csv_file = args.csv_file
-    root_dir = args.root_dir
-    num_classes = args.num_classes
-    path_to_model = args.path_to_model
-    
-    train_loader, val_loader = create_dataloader(
-        csv_file=csv_file,
-        root_dir=root_dir,
-        num_classes=num_classes
-    )
-    if path_to_model:
-        model = torch.load(path_to_model)
-    else:
-        model = torchvision.models.resnext50_32x4d(pretrained=True)
-        model= FineTuneModel(model, num_classes=num_classes)
-    model = ModelInterface(model=model)
-    model.train(train_loader, val_loader) 
