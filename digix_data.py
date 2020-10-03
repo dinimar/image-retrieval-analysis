8133beb6fc8b4d99dec0d6979c3c6e3fcf6cd43d
Dinir Imameev <dinir.imameev@gmail.com>
2020-10-03 11:06:47 +0300

    feat(digix_data): update load func

--- digix_data.py
+++ digix_data.py
@@ -2,9 +2,9 @@ import yaml
 import wget
 import os
 
-def download_datasets(yaml_path):
-    with open(os.path.join(yaml_path, 'digix.yaml')) as f:
-        data = yaml.load(f, Loader=yaml.FullLoader)
+def download_datasets(yaml_dir):
+    with open(os.path.join(yaml_dir, 'digix.yaml')) as f:
+        data = yaml.safe_load(f)
         
         wget.download(data['train_data'])
         wget.download(data['test_data'])
